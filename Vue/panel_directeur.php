c1357981436c1f6f84ff72dd012074b478669835
abel2319 <abel.kpohinto@imsp-uac.org>
2022-06-30 17:34:18 +0100

    qlq amelioration des vues

---
+++ Vue/panel_directeur.php
@@ -0,0 +1,38 @@
+<!Doctype html>
+<html>
+<head>
+<title>Gestion Parcours Etudiant </title>
+
+<link rel = "stylesheet" type= "text/css" href = "../Styles/Bases/reset.css"/>
+<link rel = "stylesheet" type= "text/css" href = "../Styles/Modules/panel.css"/>
+<link rel = "stylesheet" type= "text/css" href = "../Styles/Bases/base.css"/>
+<link rel = "stylesheet" type= "text/css" href = "../Styles/Layouts/panel_layaouts.css"/>
+
+</head>
+<body>
+    <div class="block" id="block">
+        <nav class = "menu">
+            <div class="user">
+                <div class="user_img"><img src = "images/profil.jpg" class="profil"></div>
+                <p class="user_mail">directuer@imsp-uac.org</p>
+            </div>
+            <ul class="liens_gauche"> 
+                <li class="accueil"><a href="sco_home.php">Accueil</a></li> 
+                <li class="inscription"><a href="recherche.php">Recherche</a></li>
+                <li class="notes"><a href="note.php">Notes</a></li>
+                <li class="filiere"><a href="filiere.php">Filière</a></li>
+            </ul>
+            <div class="logo">
+                <img src = "images/logo.jpeg"x>
+            </div>
+        </nav> 
+    </div>
+    <div class="header_block" id="header_block">
+        <p class="header_text">Welcome to your Dashboard</p>
+        <div class="log_btn"><button type="submit" name="bouton" formaction="../index.php" autofocus>log out</button></div>
+    </div>
+    <div class="center_block" id="center_block">
+        <p>just to see all</p>
+    </div>
+  </body>
+</html>
