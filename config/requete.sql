96539d47f464daf6a1596db05b6dfe81afee25ff
Megnigbeto Amédée <medee.megni.gbeto@protonmail.ch>
2022-06-30 10:20:50 +0100

    MOdification du fichier requete.sql

--- config/requete.sql
+++ config/requete.sql
@@ -14,7 +14,8 @@ CREATE TABLE Matiere
 (
 	id INT PRIMARY KEY,
 	nom VARCHAR(30) NOT NULL,
-	credit SMALLINT NOT NULL CHECK(credit > 0)
+	credit SMALLINT NOT NULL CHECK(credit > 0),
+	
 );
 
 CREATE TABLE UE
@@ -28,10 +29,11 @@ CREATE TABLE UE
 CREATE TABLE Filiere
 (
 	id INT,
-    nom VARCHAR(30) NOT NULL,
+   	nom VARCHAR(30) NOT NULL,
 	responsable_filiere VARCHAR(90) NOT NULL,
 	id_UE INT REFERENCES UE(id),
-	PRIMARY KEY (id, id_UE)
+	id_Etudiant INT REFERENCES Etudiant(matricule),
+	PRIMARY KEY (id, id_UE, id_Etudiant)
 );
 
 CREATE TABLE Etudiant
@@ -47,7 +49,7 @@ CREATE TABLE Etudiant
 	contact VARCHAR(30) NOT NULL,
 	adresse VARCHAR(30) NOT NULL,
 	photo VARCHAR(60) NOT NULL,
-	id_Filiere INT REFERENCES Filiere(id)
+
 );
 
 CREATE TABLE ChefScolarite
@@ -58,40 +60,37 @@ CREATE TABLE ChefScolarite
 	date_naissance DATE NOT NULL,
 	lieu_naissance VARCHAR(30) NOT NULL,
 	nationalite VARCHAR(30) NOT NULL,
-	email VARCHAR(30),
+	email VARCHAR(30) NOT NULL,
 	genre VARCHAR(30) NOT NULL,
 	contact VARCHAR(30) NOT NULL,
 	adresse VARCHAR(30) NOT NULL,
-	mot_de_passe VARCHAR(50) NOT NULL
+	mot_de_passe VARCHAR(50) NOT NULL,
+	id_Etudiant INT REFERENCES Etudiant(matricule),
+	PRIMARY KEY (id, id_Etudiant)
 );
 
 CREATE TABLE Directeur
 (
-	id INT PRIMARY KEY,
+	id INT NOT NULL,
 	nom VARCHAR(30) NOT NULL,
 	prenom VARCHAR(60) NOT NULL,
 	date_naissance DATE NOT NULL,
 	lieu_naissance VARCHAR(30) NOT NULL,
 	nationalite VARCHAR(30) NOT NULL,
-	email VARCHAR(30),
+	email VARCHAR(30) NOT NULL,
 	genre VARCHAR(30) NOT NULL,
 	contact VARCHAR(30) NOT NULL,
 	adresse VARCHAR(30) NOT NULL,
-	mot_de_passe VARCHAR(50) NOT NULL
+	mot_de_passe VARCHAR(50) NOT NULL,
+	id_Etudiant INT REFERENCES Etudiant(matricule),
+	PRIMARY KEY (id, id_Etudiant)
 );
 
-CREATE TABLE Etudiant_has_note_in_Matiere
+CREATE TABLE EtudiantHasNoteInMatiere
 (
 	note REAL CHECK (note >= 0. AND note <= 20.),
 	note_ratrappage REAL CHECK (note_ratrappage >= 0. AND note_ratrappage <= 20.),
-	id_Etudiant INT REFERENCES Etudiant(id),
+	id_Etudiant INT REFERENCES Etudiant(matricule),
 	id_Matiere INT REFERENCES Matiere(id),
 	PRIMARY KEY (id_Etudiant, id_Matiere)
-);
-
-CREATE TABLE ChefScolarite_manage_Etudiant
-(
-	id_ChefScolarite INT REFERENCES ChefScolarite(id),
-	id_Etudiant INT REFERENCES Etudiant(matricule),
-	PRIMARY KEY (id_ChefScolarite, id_Etudiant)
 );
