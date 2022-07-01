15f860398dc3550d901e08375e2ecf56edbd71f4
abel2319 <abel.kpohinto@imsp-uac.org>
2022-07-01 10:21:10 +0100

    all

---
+++ Controlleur/Inscription/infofiliere.php
@@ -0,0 +1,9 @@
+<?php
+echo
+'<datalist id="course_study">
+      <option value="1">fffffffffff</option>
+      <option value="2"></option>
+      <option value="3"></option>
+ </datalist>
+ ';
+ ?>
